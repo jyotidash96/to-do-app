b5f522dc2813095fdb69f7262e0e90b421457f77
jyotidash96 <dashjyoti450@gmail.com>
2021-03-31 21:33:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+
+import React, {useState} from "react";
 import './App.css';
+import Form from "./Components/Form";
+import TodoList from "./Components/TodoList";
 
 function App() {
+  const[inputText , setInputText] = useState(" ");
+  const[todos , setTodos] = useState([]);
+  const[status, setStatus] = useState('all');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <Form inputText={inputText}  setInputText={setInputText} todos={todos} setTodos={setTodos}  setStatus={setStatus} status={status}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
